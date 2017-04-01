b322d190a30a1a1cf02aeed1989a0d7cfb085891
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-04-01 19:21:18 +0100

    Added getting the values form the fields

--- Studdler/app/src/main/java/com/example/studdler/NewExam.java
+++ Studdler/app/src/main/java/com/example/studdler/NewExam.java
@@ -1,9 +1,13 @@
 package com.example.studdler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class NewExam extends AppCompatActivity {
 
@@ -22,5 +26,30 @@ public class NewExam extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
         s.setAdapter(adapter);
+    }
+
+
+    public void saveNewExam(View view) {
+
+
+        EditText name_field   = (EditText)findViewById(R.id.editText);
+        EditText grade_field   = (EditText)findViewById(R.id.editText2);
+        EditText min_field   = (EditText)findViewById(R.id.min_hours);
+        EditText max_field   = (EditText)findViewById(R.id.max_hours);
+        Spinner s = (Spinner)findViewById(R.id.spinner);
+
+
+        String difficulty = s.getSelectedItem().toString();
+        String name = name_field.getText().toString();
+        String grade = grade_field.getText().toString();
+        String min = min_field.getText().toString();
+        String max = max_field.getText().toString();
+
+
+        Toast toast = Toast.makeText(NewExam.this, "Saved!", Toast.LENGTH_SHORT);
+        toast.show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
